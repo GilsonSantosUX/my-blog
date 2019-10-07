de5a2794994bb0af70e73d9f71f1373be35c5737
Gilson Santos <gilsonjosert@gmail.com>
2019-10-07 05:22:34 +0000

    PostItem

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -2,10 +2,12 @@ import styled from 'styled-components'
 
 export const LayoutWarpper = styled.section `
   display:flex;
+  flex-direction:row;
 `
 
 export const LayoutMain = styled.main `
   background:#16202c;
+  width:100%;
   min-height:100vh;
-  padding:0 3.75rem 0 20rem;
+  padding:0 3.75rem 0 24rem;
  `
