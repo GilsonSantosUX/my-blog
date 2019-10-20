2a6ab6c4373b27d5fc5bc80e3e6e1003d3e44cc1
Gilson Santos <gilsonjosert@gmail.com>
2019-10-20 21:49:55 +0000

    Pages

--- gatsby-node.js
+++ gatsby-node.js
@@ -26,19 +26,28 @@ exports.onCreateNode = ({ node, getNode, actions }) => {
 exports.createPages = ({ graphql, actions }) => {
   const { createPage } = actions
   return graphql(`
-    {
-      allMarkdownRemark {
-        edges {
-          node {
-            fields {
-              slug
+    query PostItem {
+        allMarkdownRemark(sort: {fields: frontmatter___date, order: DESC},) {
+            edges {
+            node {
+                fields {
+                slug
+                }
+                frontmatter {
+                title
+                date(formatString: "DD [de] MMMM [de] YYYY", locale: "pt-br")
+                description
+                category
+                color
+                }
+                timeToRead
             }
-          }
+            }
         }
-      }
     }
   `).then(result => {
-    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    const posts = result.data.allMarkdownRemark.edges
+    posts.forEach(({ node }) => {
       createPage({
         path: node.fields.slug,
         component: path.resolve(`./src/templates/blog-post.js`),
@@ -49,5 +58,22 @@ exports.createPages = ({ graphql, actions }) => {
         },
       })
     })
+
+    const postsPerPage = 4
+    const numPages = Math.ceil(posts.length / postsPerPage)
+
+    Array.from({ length: numPages }).forEach((_, index) => {
+      createPage({
+        path: index === 0 ? `/` : `/page/${index + 1}`,
+        component: path.resolve(`./src/templates/blog-list.js`),
+        context: {
+          limit: postsPerPage,
+          skip: index * postsPerPage,
+          numPages,
+          currentPage: index + 1,
+        },
+      })
+    })
+
   })
 }
