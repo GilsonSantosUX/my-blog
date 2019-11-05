39c73b8e8fa9c038668c071f15faf75e198edb6c
Gilson Santos <gilsonjosert@gmail.com>
2019-11-05 17:14:47 +0000

    Implements SearchPage

---
+++ src/components/Search/styled.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components'
+
+export const SearchWrapper = styled.section`
+    background:${props => props.bg };
+    width:100%;
+    height:100%;
+    color: #fff;
+    padding:2rem 3rem;
+    box-sizing:border-box;
+`
